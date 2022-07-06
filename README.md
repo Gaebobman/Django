5b81e4a21857681d89d96fd9aa927f0341b4e331
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-07-06 22:00:46 +0900

    07.06

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 <ul>
 <li> Routing URLConf</li>
 </ul>
+
+`22.07.06
+<ul>
+<li> GET/POST</li>
+<li> Query String</li>
+<li> Request response object</li>
+<li> Redirection</li>
+</ul>
--- firstapp/urls.py
+++ firstapp/urls.py
@@ -7,5 +7,7 @@ from firstapp import views
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
-    path('read/<id>/', views.read)
+    path('read/<id>/', views.read),
+    path('update/<id>/', views.update),
+    path('delete/', views.delete)
 ]
