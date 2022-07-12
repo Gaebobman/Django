eb0a1cb0e8c58648a5f7ba5b98bddfd542a3fb96
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-07-12 21:28:43 +0900

    07.12 CBV

--- Book/blog/views.py
+++ Book/blog/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+# CBV
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+# index() 와 동일한 역할
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
 
+
+
+
+
+# FBV
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+#
+#
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
