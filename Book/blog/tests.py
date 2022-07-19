8e6436c013eef7e1ed757c165b93c2feaa96d969
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-07-19 19:52:09 +0900

    07.19 포스트 없는 경우 테스트

--- Book/blog/tests.py
+++ Book/blog/tests.py
@@ -17,17 +17,23 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # 페이지 타이틀이 Blog인지 확인
-        soup = BeautifulSoup(response.context, 'html.parser')
+        # HTML 요소에 접근하기 쉽게 하기 위해 Beautifulsoup 으로 읽어옴
+        # html.parser 명령어로 파싱한 결과를 soup에 담음.
+        # 그 후 텍스트만 가져와서 텍스트가 Blog 인지 확인
+        soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
 
         # 네비게이션 바를 가져옴
         navbar = soup.nav
-        # 네비게이션 바 안에 컨텐츠 들을 확인한다
+        # 네비게이션 바 안의 컨텐츠 들을 확인한다
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
-        # 포스트가 하나도 없다면
+        # 작성된 포스트가 0개 인지 확인
+        # 테스트 시작시 테스트용 데이터 베이스를 생성함, 단 setUp() 함수 에서 설정한 요소는 포함 시킴
+        # 그런데 setUp() 함수에서 데이터베이스 관련 조작을 하지 않았으므로 DB 에는 아무것도 없음
         self.assertEqual(Post.objects.count(), 0)
+
         # main area 에 아직 게시물이 없습니다 라는 문구가 나타남
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
@@ -43,7 +49,8 @@ class TestView(TestCase):
         )
         self.assertEqual(Post.objects.count(), 2)
 
-        # 포스트 목록 페이지를 새로고침 했을 때
+        # 포스트 목록 페이지를 새로고침 했을 때를 확인
+        # 페이지를 새로고침하는 코드임
         response = self.client.get('/blog/')
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
