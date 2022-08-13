0fee783927d19b0f9c6411f36f84cc0d6e763c52
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-08-13 15:54:26 +0900

    08.13 포스트 수정기능 추가

--- Book/blog/tests.py
+++ Book/blog/tests.py
@@ -10,7 +10,9 @@ class TestView(TestCase):
         self.client = Client()
         self.user_naver = User.objects.create_user(username='naver', password='passwooord')
         self.user_kakao = User.objects.create_user(username='kakao', password='passwooord')
-
+        self.user_naver.is_staff = True
+        self.user_naver.save()
+        
         self.category_law = Category.objects.create(name='law', slug='law')
         self.category_engineering = Category.objects.create(name='engineering', slug='engineering')
 
@@ -189,8 +191,13 @@ class TestView(TestCase):
         response = self.client.get('/blog/create_post/', follow=True)
         self.assertNotEqual(response.status_code, 200)
 
+        # Staff 권한 없는 사용자로 로그인
+        self.client.login(username='kakao', password='passwooord')
+        response = self.client.get('/blog/create_post/', follow=True)
+        self.assertNotEqual(response.status_code, 200)
+        
+        # Staff 권한이 있는 사용자로 로그인
         self.client.login(username='naver', password='passwooord')
-
         response = self.client.get('/blog/create_post/', follow=True)
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -209,3 +216,47 @@ class TestView(TestCase):
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username,'naver')
+
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
+
+        # 로그인 하지 않은 경우
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.status_code, 200)
+
+        # 로그인은 했지만 포스트 소유자가 아닌 경우
+        self.assertNotEqual(self.post_003.author, self.user_naver)
+        self.client.login(
+            username=self.user_naver.username,
+            password='passwooord'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+
+        # 포스트 작성자가 접근하는 경우
+        self.client.login(
+            username=self.post_003.author.username,
+            password='passwooord'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup. find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                'title': '세 번째 포스트를 수정했습니다. ',
+                'content': '헬로 월드 월드 월드콘',
+                'category': self.category_law.pk
+            },
+            follow=True
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
+        self.assertIn('헬로 월드 월드 월드콘', main_area.text)
+        self.assertIn(self.category_law.name, main_area.text)
